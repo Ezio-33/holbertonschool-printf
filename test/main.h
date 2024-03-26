be37e6ec1723d24582962cc533bba8c818f2a71f
Ezio-33 <8691@holbertonstudents.com>
2024-03-26 12:23:17 +0000

    mise a jour du fichier main.h

--- test/main.h
+++ test/main.h
@@ -19,8 +19,31 @@ int _putchar(char c)
 	return (write(1, &c, 1));
 }
 
+/**
+ * print_string - Afficher une chaîne de caractères
+ * dans la sortie standard (stdout)
+ * @str_value: Le texte à afficher
+ * Return: Nombre de caractères affichés
+ */
+int print_string(char *str_value)
+{
+	int i = 0;
+
+	while (*str_value != '\0')
+	{
+		_putchar(*str_value);
+		str_value++;
+		i++;
+	}
+	return (i);
+}
+
+/****************** FUNCTIONS ******************/
+
 int _printf(const char *format, ...);
 int _putchar(char c);
+int print_string(char *str_value);
 int formatage_printf(va_list arg_l, char nextChar);
+char *_itoa(int num, int base);
 
 #endif
