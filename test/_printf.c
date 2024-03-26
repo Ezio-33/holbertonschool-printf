750ff51e65158e903a11d8751dd0856af5c8862b
Ezio-33 <8691@holbertonstudents.com>
2024-03-26 11:59:13 +0000

    creation fichier utile.c et mise a jour du code _printf.c

--- test/_printf.c
+++ test/_printf.c
@@ -1,63 +1,35 @@
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 #include <stdarg.h>
-#include "printf.h"
-
+#include "main.h"
 /**
- *  _printf() : Cette fonction permet de simuler le comportement de la fonction
- * printf.
- * return: Retourne le nombre de caractères affichés.
+ * _printf - Mettre en forme un texte et l'afficher
+ * @format: Chaîne originale à formater
+ *
+ * Return: Nombre de caractères affichés
  */
 int _printf(const char *format, ...)
 {
-	va_list args;
-	va_start(args, format);
-	int count = 0;
+	int printedChars = 0;
+	char currentChar;
+	va_list arg_l;
+
+	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+		exit(1);
+	va_start(arg_l, format);
 	while (*format != '\0')
 	{
-		if (*format == '%')
+		currentChar = *format;
+		format++;
+		if (currentChar == '%')
 		{
-			format++;
+			char nextChar = *format++;
 
-			if (*format == 'c')
-			{
-				char c = va_arg(args, int);
-				_putchar(c);
-				count++;
-			}
-			else if (*format == 's')
-			{
-				char *s = va_arg(args, char *);
-				while (*s != '\0')
-				{
-					_putchar(*s);
-					s++;
-					count++;
-				}
-			}
-			else if (*format == '%')
-			{
-				_putchar('%');
-				count++;
-			}
-			else if (*format == 'd' || *format == 'i')
-			{
-				int value = va_arg(args, int);
-				char buffer[20];
-				int len = sprintf(buffer, "%d", value);
-				for (int i = 0; i < len; i++)
-					_putchar(buffer[i]);
-				count += len;
-			}
+			printedChars += handle_format(arg_l, nextChar);
 		}
 		else
-		{
-			_putchar(*format);
-			count++;
-		}
-
-		format++;
+			printedChars += _putchar(currentChar);
 	}
-
-	va_end(args);
-
-	return (count);
+	return (printedChars);
 }
---
+++ test/utile.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdarg.h>
+#include "main.h"
+/**
+ * handle_format - Gérer le formatage printf
+ * @arg_l: va_list de la fonction printf
+ * @nextChar: Char correspondant au type de format
+ * Return: Nombre de caractères affichés
+ */
+int handle_format(va_list arg_l, char nextChar)
+{
+	int printedChars = 0;
+
+	if (nextChar == '%')
+	{
+		printedChars += _putchar(nextChar);
+	}
+	else if (nextChar == 'd' || nextChar == 'i')
+	{
+		printedChars += print_string(_itoa(va_arg(arg_l, int), 10));
+	}
+	else if (nextChar == 'c')
+	{
+		printedChars += _putchar((int)va_arg(arg_l, int));
+	}
+	else if (nextChar == 's')
+	{
+		char *text = va_arg(arg_l, char *);
+
+		if (!text)
+			text = "(null)";
+		printedChars += print_string(text);
+	}
+	else
+		printedChars += (_putchar('%') + _putchar(nextChar));
+	va_end(arg_l);
+	return (printedChars);
+}
