5a1f4336f3e211291beb7f737298569a12f7ee74
Ezio-33 <8691@holbertonstudents.com>
2024-03-26 12:33:26 +0000

    Ajout des fonctions sur le main.h

--- test/main.h
+++ test/main.h
@@ -43,7 +43,9 @@ int print_string(char *str_value)
 int _printf(const char *format, ...);
 int _putchar(char c);
 int print_string(char *str_value);
-int formatage_printf(va_list arg_l, char nextChar);
+int handle_format(va_list arg_l, char nextChar);
+int _abs(int n);
 char *_itoa(int num, int base);
+char *_reverse(char *s, int n);
 
 #endif
